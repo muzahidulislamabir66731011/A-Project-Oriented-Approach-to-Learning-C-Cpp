c835299dd3af79c14fa3e8f4f1146096204c2ddf
MUZAHIDUL ISLAM ABIR <empoweredrandd@gmail.com>
2025-12-01 06:31:38 +0600

    Create ex11.c

---
+++ simple problems(syntax based)/ex11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix[5][5] = {
+      4, 6, 25, 88, 5,
+      34, 5, 300, 4, 65,
+      78, 43, 11, 90, 125,
+      98, 585, 12, 63, 21,
+      45, 35, 9, 5, 1
+   };
+    printf("In the array:\n");
+    int sum=0;
+    int i,j;
+    for(i=0;i<=4;i++){
+        for(j=0;j<=4;j++){
+            sum+=matrix[i][i];
+            if (j==4){
+                printf(" %d\n",matrix[i][j]);
+            } else{
+                printf(" %d",matrix[i][j]);
+            }
+        }  
+    }
+    printf("\nthe sum of the elements is %d", sum);
+    return 0;
+}
